6ebf3a8acc3265974aa85b405084ee654a558ff5
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-03-10 17:28:40 +0100

    Add card filtr

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,10 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForList = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForList = ({cards, searchString}, columnId) => cards.filter(
+  card => card.columnId == columnId 
+  && new RegExp(searchString, 'i').test(card.title)
+);
 
 // action name creator
 const reducerName = 'cards';
@@ -11,7 +14,9 @@ const createActionName = name => `app/${reducerName}/${name}`;
 export const ADD_CARD = createActionName('ADD_CARD');
 
 // action creators
-export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
+export const createActionAddCard = payload => (
+  { payload: { ...payload, id: shortid.generate() }, type: ADD_CARD }
+);
 
 // reducer
 export default function reducer(state = [], action = {}) {
